02c6d74d7d8ed6b3af920b79811b17932a65dafb
Noe Jupiter <noeonjupiter@icloud.com>
2022-05-08 12:37:22 +0300

    .

--- Sources/Model-SwiftUI-VM/BaseView/BaseViews.swift
+++ Sources/Model-SwiftUI-VM/BaseView/BaseViews.swift
@@ -10,19 +10,18 @@ import SwiftUI
 public extension BaseView {
     var body: some View {
         GeometryReader { proxy in
-//            Configurations.shared.body!(self)
-//            if let contentBody = Configurations.shared.body {
-//                contentBody(self)
-//                    .bindColors(colorScheme)
-//#if canImport(AppKit)
-//                    .bind(proxy: proxy, viewModel: viewModel, bindable: !size)
-//#endif
-//            }else {
-//                EmptyView()
-//                    .onAppear {
-//                        fatalError("You should set the content of the Configurations using Configurations.makeBody")
-//                    }
-//            }
+            if let contentBody = Configurations.shared.body {
+                contentBody(self)
+                    .bindColors(colorScheme)
+#if canImport(AppKit)
+                    .bind(proxy: proxy, viewModel: viewModel, bindable: !size)
+#endif
+            }else {
+                EmptyView()
+                    .onAppear {
+                        fatalError("You should set the content of the Configurations using Configurations.makeBody")
+                    }
+            }
         }.onAppear {
             print(Configurations.shared.body(self))
         }
