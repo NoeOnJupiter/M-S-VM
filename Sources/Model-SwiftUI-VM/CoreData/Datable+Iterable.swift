e72caa29118d567d9efb926b827b5a54e5537393
Noe Jupiter <noeonjupiter@icloud.com>
2022-07-17 19:14:13 +0300

    Iterable

---
+++ Sources/Model-SwiftUI-VM/CoreData/Datable+Iterable.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joe Maghzal on 7/17/22.
+//
+
+import Foundation
+import CoreData
+
+public extension Datable where Self: Iterable {
+//    static func map(from object: Object?) -> Self? {
+//        guard let properties = try? allProperties() else {
+//            return nil
+//        }
+//    }
+    func getObject(from object: Object, isUpdating: Bool) -> Object {
+        guard let properties = try? allProperties() else {
+            return object
+        }
+        properties.forEach { property in
+            var value = property.value
+            let objectType = object.value(forKey: property.key)
+            if property.key == "oid" {
+                let newValue = value as? UUID ?? UUID()
+                value = newValue
+            }else if let datableValue = value as? (any Datable), objectType is NSManagedObject {
+                if datableValue.oid == nil || oid == nil {
+                    object.setValue(datableValue.object, forKey: property.key)
+                }else {
+                    object.setValue(datableValue.updatedObject, forKey: property.key)
+                }
+            }else if let datableValue = value as? Array<(any Datable)>, objectType is NSSet {
+                let set = NSSet(array: datableValue.map { subValue in
+                    if subValue.oid == nil || oid == nil{
+                       return subValue.object
+                    }
+                    return subValue.updatedObject
+                })
+                object.setValue(set, forKey: property.key)
+            }
+            object.setValue(value, forKey: property.key)
+        }
+        return object
+    }
+}
