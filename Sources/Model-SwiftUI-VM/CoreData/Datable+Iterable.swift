094a373144bdf119af7ed12a4cc8f400c17344b0
Noe Jupiter <noeonjupiter@icloud.com>
2022-07-17 19:36:03 +0300

    Iterable

--- Sources/Model-SwiftUI-VM/CoreData/Datable+Iterable.swift
+++ Sources/Model-SwiftUI-VM/CoreData/Datable+Iterable.swift
@@ -24,13 +24,13 @@ public extension Datable where Self: Iterable {
             if property.key == "oid" {
                 let newValue = value as? UUID ?? UUID()
                 object.setValue(newValue, forKey: property.key)
-            }else if let datableValue = value as? (any Datable), objectType is NSManagedObject {
+            }else if let datableValue = value as? (any Datable) {
                 if datableValue.oid == nil || oid == nil {
                     object.setValue(datableValue.object, forKey: property.key)
                 }else {
                     object.setValue(datableValue.updatedObject, forKey: property.key)
                 }
-            }else if let datableValue = value as? Array<(any Datable)>, objectType is NSSet {
+            }else if let datableValue = value as? Array<(any Datable)> {
                 let set = NSSet(array: datableValue.map { subValue in
                     if subValue.oid == nil || oid == nil{
                        return subValue.object
